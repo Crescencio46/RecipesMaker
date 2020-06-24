1d9ce2f46a633abfdc166978824b1dfb36133c64
Crescencio <crescen46@outlook.com>
2020-06-24 19:15:02 +0200

    dynamic options

--- src/RecipesMaker.kt
+++ src/RecipesMaker.kt
@@ -1,8 +1,9 @@
+val ingredients = listOf("Water", "Milk", "Meat", "Vegetables", "Fruits", "Cereals", "Eggs", "Oil")
+
 fun main(){
 
-    val ingredients = listOf("Water", "Milk", "Meat", "Vegetables", "Fruits", "Cereals", "Eggs", "Oil")
-
     val menu: String = """
+        
         ::Welcome to Recipe Maker::
         
         Select wish option:
@@ -12,15 +13,39 @@ fun main(){
     """.trimIndent()
 
     println(menu)
-    val response: String? = readLine()
 
-    when (response) {
-        "1" -> println("Make Recipe \nChoose you ingrediends")
-        "2" -> println("Look my recipes \nEmpty")
-        "3" -> println("Exit \nThanks for useing this app")
-        else 
-        //println("Please, choose one option")
+
+    do {
+
+        val response: String? = readLine()
+
+        when (response) {
+            "1" -> makeRecipe() //println("Make Recipe \nChoose you ingredients")
+            "2" -> viewRecipe() //println("Look my recipes \nEmpty")
+            "3" ->  println("Exit \nThanks for using this app")
+            else -> println("Choose a valid option")
+        }
+    }while (response != "3")
+
+
+}
+
+fun makeRecipe(){
+
+    val options = """
+        
+        ::Hacer receta::
+        
+        Selecciona los ingredientes deseados:
+
+    """.trimIndent()
+
+    println(options)
+    ingredients.forEach{
+        println(it)
     }
+}
 
-
+fun viewRecipe(){
+    println("My recipes")
 }
